aa6e2ebf78c0e78f26a985bc72a9e7087e688029
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-11-28 19:11:35 +0100

    CRUD Product
    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- esprit-shop/esprit-shop-web/src/main/java/mBeans/ProductBean.java
+++ esprit-shop/esprit-shop-web/src/main/java/mBeans/ProductBean.java
@@ -14,6 +14,10 @@ import services.ProductServicesLocal;
 @ManagedBean
 @ViewScoped
 public class ProductBean {
+	private Boolean displayF1 = true;
+	private Boolean displayF2 = false;
+
+	private Product product = new Product();
 	private List<Product> products = new ArrayList<>();
 	@EJB
 	private ProductServicesLocal productServicesLocal;
@@ -23,6 +27,17 @@ public class ProductBean {
 	public List<Product> getProducts() {
 		products = productServicesLocal.findProductsByCompany(identity.getUser().getId());
 		return products;
+	}
+
+	public void select() {
+		displayF1 = false;
+		displayF2 = true;
+	}
+
+	public void cancel() {
+		displayF1 = true;
+		displayF2 = false;
+		product = new Product();
 	}
 
 	public void setProducts(List<Product> products) {
@@ -35,6 +50,30 @@ public class ProductBean {
 
 	public void setIdentity(Identity identity) {
 		this.identity = identity;
+	}
+
+	public Boolean getDisplayF1() {
+		return displayF1;
+	}
+
+	public void setDisplayF1(Boolean displayF1) {
+		this.displayF1 = displayF1;
+	}
+
+	public Boolean getDisplayF2() {
+		return displayF2;
+	}
+
+	public void setDisplayF2(Boolean displayF2) {
+		this.displayF2 = displayF2;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
 	}
 
 }
